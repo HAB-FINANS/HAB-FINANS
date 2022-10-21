c38989a37036805a33a766778f81b9a30ac65ea3
Andres Felipe Mendoza <ibaguetolima@hotmail.com>
2022-10-20 19:40:26 -0500

    conexion de bd OK para create

--- src/main/java/com/habfinans/habfinans/controllers/EnterpriseController.java
+++ src/main/java/com/habfinans/habfinans/controllers/EnterpriseController.java
@@ -1,6 +1,7 @@
 package com.habfinans.habfinans.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,16 +25,24 @@ public class EnterpriseController {
     public String enterprise_admin(){
         return "gestion_empresas";
     } */
+    @PostMapping({"/enterprise_admin"})
+    public RedirectView crearEmpresa(@ModelAttribute @DateTimeFormat(pattern = "yyyy-MM-DD") Enterprise enterprise, Model model){
+        model.addAttribute(enterprise);
+        this.service.createEnterprise(enterprise);
+        return new RedirectView("/enterprise_admin");
+    }
 
     @GetMapping({"/new_enterprise"})
-    public String formularioNuevoEmpleado(Model model){
+    public String formularioNuevoEmpresa(Model model){
         model.addAttribute("enterprise", new Enterprise());
         return "crear_empresa";
     }
 
     @GetMapping({"/enterprise_admin"})
-    public String listarEmpleados(Model model){
+    public String listarEmpresa(Model model){
         model.addAttribute("enterprises", service.getEnterprise());
         return "gestion_empresas";
     }
+
+
 }
--- src/main/java/com/habfinans/habfinans/entities/Enterprise.java
+++ src/main/java/com/habfinans/habfinans/entities/Enterprise.java
@@ -6,7 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
+import java.time.LocalDate;
 
 @Entity
 @Table(name="enterprises")
@@ -31,66 +31,108 @@ public class Enterprise {
     private String phone;
 
     @Column(name="created_at")
-    private String created_at;
+    private LocalDate created_at;
 
     @Column(name="updated_at")
-    private String updated_at;
+    private LocalDate updated_at;
+
+    
+/* 
+    public Enterprise(String name, String nit, String address, String phone, LocalDate created_at,
+    LocalDate updated_at) {
+        this.name = name;
+        this.nit = nit;
+        this.address = address;
+        this.phone = phone;
+        this.created_at = created_at;
+        this.updated_at = updated_at;
+    } */
+
+
 
     public Long getId() {
         return id;
     }
 
+
+
     public void setId(Long id) {
         this.id = id;
     }
+
+
 
     public String getName() {
         return name;
     }
 
+
+
     public void setName(String name) {
         this.name = name;
     }
+
+
 
     public String getNit() {
         return nit;
     }
 
+
+
     public void setNit(String nit) {
         this.nit = nit;
     }
+
+
 
     public String getAddress() {
         return address;
     }
 
+
+
     public void setAddress(String address) {
         this.address = address;
     }
+
+
 
     public String getPhone() {
         return phone;
     }
 
+
+
     public void setPhone(String phone) {
         this.phone = phone;
     }
 
-    public String getCreated_at() {
+
+
+    public LocalDate getCreated_at() {
         return created_at;
     }
 
-    public void setCreated_at(String created_at) {
+
+
+    public void setCreated_at(LocalDate created_at) {
         this.created_at = created_at;
     }
 
-    public String getUpdated_at() {
+
+
+    public LocalDate getUpdated_at() {
         return updated_at;
     }
 
-    public void setUpdated_at(String updated_at) {
+
+
+    public void setUpdated_at(LocalDate updated_at) {
         this.updated_at = updated_at;
     }
+
+
     
     
 
